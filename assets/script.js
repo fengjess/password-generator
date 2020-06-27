2bc0e551ba3ed881ac4647761763d370075fe9f8
Jessica Feng <jessicafeng14@yahoo.com>
2020-06-26 22:50:24 -0700

    created javascript and added variables

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_-+{}|[]\;':.//?><`~";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "1234567890";
+var specialChar = "!@#$%^&*()_-+={}|[]\;':.,/?><`~"
