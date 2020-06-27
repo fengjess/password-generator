9033932ee12a92fd063f3c01db05fd1215e00c12
Jessica Feng <jessicafeng14@yahoo.com>
2020-06-27 01:07:49 -0700

    linked functions to generate password button

--- assets/script.js
+++ assets/script.js
@@ -5,20 +5,43 @@ var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "1234567890";
-var specialChar = "!@#$%^&*()_-+={}|[]\;':.,/?><`~"
+var specialChar = "!@#$%^&*()_-+={}|[]\;':.,/?><`~";
 var password = "";
+var tempValues = "";
 
 
+genPasswordBtn.addEventListener("click", function () {
+    var passwordLen = prompt("How many characters would you like your password to contain?");
 
-var passwordLen = prompt("How many characters would you like your password to contain?");
-if (passwordLen < 8 || passwordLen > 128) {
-    alert("Password must be between 8 - 128 characters")
-}
+    if (passwordLen < 8 || passwordLen > 128) {
+        alert("Password must be between 8 - 128 characters")
+        return;
+    }
 
+    if (confirm("Add uppercase letters?")) {
+        tempValues = tempValues + upperCase;
+    }
 
+    if (confirm("Add lowercase letters?")) {
+        tempValues = tempValues + lowerCase;
+    }
+
+    if (confirm("Add numbers?")) {
+        tempValues = tempValues + numbers;
+    }
+
+    if (confirm("Add special characters?")) {
+        tempValues = tempValues + specialChar;
+    }
+});
+
+function passwordLen();
 for (var i = 0; i <= passwordLen; i++) {
-    var password = password + tempValues.charAt(Math.floor(Math,random() * Math.floor(values.length-1)));
+    password = password + tempValues.charAt(Math.floor(Math.random() * Math.floor(values.length-1)));
 }
+
+// Display generated password
+yourPassword.textContent = password;
 
 // Copy password to clipboard
 function copyPassword() {
